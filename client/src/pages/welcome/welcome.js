dd14d56ca0a0ae430a0fed0647d17b75945c5d03
VladiSVS <uladzimir.siauko@gmail.com>
2021-11-09 14:00:52 +0100

    v3

--- client/src/pages/welcome/welcome.js
+++ client/src/pages/welcome/welcome.js
@@ -1,25 +1,15 @@
 import './welcome.scss'
-import { Link } from 'react-router-dom'
+
+import Titel from '../../components/titel/titel'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <ul>
-            <li>
-                <Link to="/home">Home</Link>
-            </li>
-            <li>
-                <Link to="/yoga">Yoga</Link>
-            </li>
-            <li>
-                <Link to="/meditate">Meditate</Link>
-            </li>
-            <li>
-                <Link to="/music">Music</Link>
-            </li>
-            <li>
-                <Link to="/user">User</Link>
-            </li>
-        </ul>
+        <>
+            <Titel />
+            <div>This is Welcome!</div>
+            <Link to="/home">GO TO START</Link>
+        </>
     );
 }
 
