14e08a96b99e0dc257608398e41c88e875221184
Chenchenlin789 <chenchenlihn@gmail.com>
2021-11-09 14:13:10 +0100

    nav

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -3,7 +3,29 @@ import './nav.scss'
 const Nav = () => {
     return (
         <div>
-            This is nav
+            <nav className="">
+
+                <ul>
+                    <li>
+                        <Link to="/yoga">yoga</Link>
+                    </li>
+                    <li>
+                        <Link to="/meditation">meditation</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/music">music</Link>
+                    </li>
+                    <li>
+                        <Link to="/user">user</Link>
+                    </li>
+
+                </ul>
+
+
+            </nav>
         </div>
     );
 }
