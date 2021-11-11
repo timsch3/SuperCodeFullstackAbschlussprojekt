1536eccf3de51ebb3f5e3f879830932697a6f11d
Chenchenlin789 <chenchenlihn@gmail.com>
2021-11-11 14:48:19 +0100

    yogaDetail

---
+++ client/src/pages/details/YogaDetails.js
@@ -0,0 +1,11 @@
+const YogaDetail = () => {
+    return (
+        <div>
+            <img className="yogaDetail__icon" src="/images/yoga.png" alt=""></img>
+            <img className="yogaDetail__icon" src="/images/icons/favorites.svg" alt=""></img>
+            <img className="yogaDetail__icon" src="/images/icons/download.svg" alt=""></img>
+        </div>
+    );
+}
+
+export default YogaDetail;
