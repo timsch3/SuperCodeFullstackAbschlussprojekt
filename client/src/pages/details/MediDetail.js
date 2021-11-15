186d1fe2d454d77efcf7e1912cbd338db3647cf0
Chenchenlin789 <chenchenlihn@gmail.com>
2021-11-15 11:57:10 +0100

    meditationDetails

--- client/src/pages/details/MediDetail.js
+++ client/src/pages/details/MediDetail.js
@@ -1,41 +1,65 @@
 import './details.scss'
 import Nav from '../../components/nav/nav'
+import { Link } from 'react-router-dom'
+
+let meditate = {
+    width: "100vw",
+    height: "40vh",
+    backgroundImage: "url(/images/home/meditation.png)",
+    backgroundSize: "cover",
+    // repeat: "no-repeat"
+
+};
 
 const MediDetail = () => {
     return (
         <div>
-
-            <section style={{ backgroundImage: `url(/images/home/healthy-back.png)` }}>
-                <img className="yogaDetail__icon" src="/images/icons/arrow-back.svg" alt=""></img>
-                <img className="yogaDetail__icon" src="/images/icons/heart.svg" alt=""></img>
-                <img className="yogaDetail__icon" src="/images/icons/download.svg" alt=""></img>
+            <section style={meditate}>
+                <div className="icon__flex">
+                    <div className="yogaDetail__icon">
+                        <Link to="/user" > <img src="/images/icons/arrow-back.svg" alt=""></img></Link>
+                    </div>
+                    <div className="yogaDetail__icon2 detail__flex">
+                        <Link to="/user"> <img src="/images/icons/list/heart.svg" alt=""></img></Link>
+                        <Link to="/user"> <img src="/images/icons/download.svg" alt=""></img> </Link>
+                    </div>
+                </div>
             </section>
-            <h1>Happy Morning</h1>
-            <p>COURSE</p>
-            <p> Ease the mind into a restful night’s sleep  with
-                these deep, ambient tones.</p>
-            <img className="yogaDetail__icon" src="/images/icons/favorites.svg" alt=""></img>
-            <p>[XXX]Favorits</p>
-            <img className="yogaDetail__icon" src="/images/icons/listening.svg" alt=""></img>
-            <p>[XXX]Listening</p>
 
-            <h2>Playlist</h2>
-            <section>
-                <img className="yogaDetail__icon" src="/images/icons/.svg" alt=""></img>
+            <section className="yogaDetail__main">
+                <h1>Happy Morning</h1>
+                <p>COURSE</p>
+                <p> Ease the mind into a restful night’s sleep  with
+                    these deep, ambient tones.</p>
+            </section>
+            <section className="yogaDetail__flex_space">
+                <div className="detail__flex">
+                    <img className="yogaDetail__icon" src="/images/icons/favorites.svg" alt=""></img>
+                    <p>[XXX]Favorits</p>
+                </div>
+                <div className="detail__flex">
+                    <img className="yogaDetail__icon" src="/images/icons/listening.svg" alt=""></img>
+                    <p>[XXX]Listening</p>
+                </div>
+            </section>
+
+            <h2 className="yogaDetail__main">Playlist</h2>
+            <section className="icon__flex">
+                <img className="yogaDetail__icon" src="/images/icons/play.png" alt=""></img>
                 <div>
                     <p>Focus Attention</p>
                     <p>10min</p>
                 </div>
             </section>
-            <section>
-                <img className="yogaDetail__icon" src="/images/icons/.svg" alt=""></img>
+            <section className="icon__flex">
+                <img className="yogaDetail__icon" src="/images/icons/play.png" alt=""></img>
                 <div>
                     <p>Focus Attention</p>
                     <p>10min</p>
                 </div>
             </section>
-            <section>
-                <img className="yogaDetail__icon" src="/images/icons/.svg" alt=""></img>
+            <section className="icon__flex">
+                <img className="yogaDetail__icon" src="/images/icons/play.png" alt=""></img>
                 <div>
                     <p>Focus Attention</p>
                     <p>10min</p>
--- client/src/pages/details/YogaDetails.js
+++ client/src/pages/details/YogaDetails.js
@@ -8,7 +8,6 @@ let sectionStyle = {
     height: "60vh",
     backgroundImage: "url(/images/yoga.png)",
     backgroundSize: "cover",
-    display: "flex"
 
 };
 
@@ -17,12 +16,14 @@ const YogaDetail = () => {
         <div>
 
             <section style={sectionStyle} >
-                <div className="yogaDetail__icon">
-                    <Link to="/user" > <img src="/images/icons/arrow-back.svg" alt=""></img></Link>
-                </div>
-                <div className="yogaDetail__icon2 detail__flex">
-                    <Link to="/user"> <img src="/images/icons/list/heart.svg" alt=""></img></Link>
-                    <Link to="/user"> <img src="/images/icons/download.svg" alt=""></img> </Link>
+                <div className="icon__flex">
+                    <div className="yogaDetail__icon">
+                        <Link to="/user" > <img src="/images/icons/arrow-back.svg" alt=""></img></Link>
+                    </div>
+                    <div className="yogaDetail__icon2 detail__flex">
+                        <Link to="/user"> <img src="/images/icons/list/heart.svg" alt=""></img></Link>
+                        <Link to="/user"> <img src="/images/icons/download.svg" alt=""></img> </Link>
+                    </div>
                 </div>
             </section>
 
